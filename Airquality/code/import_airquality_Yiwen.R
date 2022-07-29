704bf4448d729b5d23086039f979a490978dd464
Marco-Felipe King <51195817+MarcoFelipeKing@users.noreply.github.com>
2022-07-29 15:34:34 +0100

    Add files via upload
    initial commit

---
+++ Airquality/code/import_airquality_Yiwen.R
@@ -0,0 +1,136 @@
+# Code to import air quality data files and create a single data.frame for plotting
+
+#Install pacman to make loading packages easier
+if("pacman" %in% rownames(installed.packages())==FALSE){install.packages("pacman"); require(pacman)}else{require(pacman)}
+
+#Load packages ----
+pacman::p_load("vroom","dplyr","tidyr","janitor","lubridate","ggplot2","fuzzyjoin")
+
+#list all files within the folders and subfolders with the .txt extension
+l <- list.files(pattern="*.txt",full.names = TRUE,recursive = TRUE)
+
+#read in each file into a list object because each file seems to have different numbers of columns
+# If we didn't have this problem of differfent number of columns, it would be super easy.
+
+
+# Read in function -----
+read_in_function <- function(filename){
+  a <- janitor::clean_names(vroom::vroom(filename))
+  return(a)
+}
+
+df_l <- lapply(X=l,FUN=read_in_function)
+
+
+# For some reason the co2_ppm column has an ; in it for some files. We need to get rid of it and convert the column to a number instead of a character ----
+
+for (i in 1:length(df_l)){
+  df_l[[i]] <- df_l[[i]] %>%
+    separate(co2_ppm,sep = ";",into=c("co2_ppm",NA)) %>%
+    mutate(co2_ppm=as.numeric(co2_ppm))
+  print(i) # Track progress
+}
+
+# rename list elements to the name of the sensor (e.g. CEN00389 and CEN00391 instead if 1, 2, 3 and 4) ----
+names(df_l) <- l %>% as_tibble() %>% separate(value,sep = "/",into=c(NA,"value",NA)) %>% pull(value)
+
+# Now we can stack them into a data.frame, creating an extra column called ID, which is taken from the list element name. We create a new object called df instead of df_l, just in case we need to refer back to the list. ------
+df <- df_l %>%
+  bind_rows(.id = "sensor")
+
+#Convert the time column to hh:mm::ss ----
+
+df <- df %>%
+  # mutate(timestamp=lubridate::as_datetime(timestamp))
+  mutate(timestamp=as.POSIXct(timestamp,origin="1970-01-01",tz = "Europe/London"))
+  # mutate(timestamp=lubridate::ymd_hms(timestamp))
+df
+
+
+# Quick  plot -----
+
+df %>%
+  filter(timestamp>"2022-07-28 10:00:00" & timestamp <"2022-07-28 16:00:00") %>%
+  ggplot()+
+  geom_point(aes(x=timestamp,y=co2_ppm,colour=sensor))+
+  scale_color_brewer(palette = "Set1")+
+  # facet_wrap(~sensor,nrow = 1)+
+  xlab("Time and date")+
+  ylab("CO2 ppm")+
+  labs(colour="Sensor")+
+  hrbrthemes::theme_ipsum()+
+  labs(title="CO2 in toilets", subtitle="CO2 ppm values during different experiments", caption="something")->p
+
+plotly::ggplotly(p)
+
+# Participant data -----
+#TODO - Create a data.frame with the start end end time of each participant
+# Use the participant start/stop time to cut out the data we doing need using left_join.
+
+Experiment_List <- data.frame(
+  participantID=c("WT_001_U","WT_001_D","WT_002_U"),
+  StartTime =c("2022-07-28 12:27:00","2022-07-28 12:40:00","2022-07-28 13:00:00"),
+  StopTime=c("2022-07-28 12:38:00","2022-07-28 12:55:00","2022-07-28 13:15:00"),
+  sex=c("F","F","F"),
+  activity=c("U","D","U")
+)
+
+# participant_meta_data <- readxl::read_xlsx(path = "participant_meta_data/participant_meta_data.xlsx",sheet = "Sheet1")
+
+
+#All the datetime values should be of type POSIXct.
+Experiment_List <- Experiment_List %>%
+  mutate(across(c(StartTime, StopTime), lubridate::ymd_hms)) 
+
+#Chunk the df into 10 bits in case we need to use mclapply
+df<-split(df, (as.numeric(rownames(df))-1) %/% 10000)
+
+#Do a left join to remove any rows not belonging to an experiment
+fuzzyJoinFunction<-function(a){
+  a<-fuzzy_left_join(a, Experiment_List, 
+                     by = c('timestamp' = 'StartTime', 'timestamp'= 'StopTime'), 
+                     match_fun = c(`>=`, `<=`))
+  a
+}
+
+df<-rbindlist(lapply(X=df,FUN=fuzzyJoinFunction))
+
+#Drop rows with no experiment - only if we don't want the time between participants.
+# df<-df %>% 
+#   drop_na(participantID)
+
+
+#TODO  - extract summary statistics for all types of experiment, and type of activity
+#TODO  - decide what to do with the offset of each sensor (i.e. that they don't all start at the same value for each experiment.)
+
+
+# Summary statistics -----
+df %>% 
+  group_by(activity) %>% 
+  summarise(M=mean(co2_ppm))
+
+# Reset the time for each participant----
+# cretea a column that just shows the number of seconds sincethe start of the experiment.
+df <- df %>% 
+  group_by(participantID, sensor) %>% 
+  mutate(measurement_time=time-min(time))
+  
+
+
+# Plot by individual -----
+df %>%
+  # filter(timestamp>"2022-07-28 10:00:00" & timestamp <"2022-07-28 16:00:00") %>%
+  ggplot()+
+  geom_point(aes(x=measurement_time,y=co2_ppm,colour=sensor))+
+  scale_color_brewer(palette = "Set1")+
+  facet_wrap(activity~participantID)+
+  xlab("Time and date")+
+  ylab("CO2 ppm")+
+  labs(colour="Sensor")+
+  hrbrthemes::theme_ipsum()+
+  labs(title="CO2 in toilets", subtitle="CO2 ppm values during different experiments", caption="something")
+
+
+
+# Calculate the decay ventilation curve after the participant has left.
+
